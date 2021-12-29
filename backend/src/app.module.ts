9a0ad2690f95c46143738b8ea922ab7f90031a44
Maryucha <61298972+Maryucha@users.noreply.github.com>
2021-12-29 13:46:27 -0300

    feat: add commitlint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
+import { winstonConfig } from './configs/winston.config';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 
 @Module({
   imports: [ 
@@ -16,8 +20,13 @@ import { BookModule } from './book/book.module';
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: (process.env.DB_SYNCHRONIZE === 'true'),
-    }),AuthorModule, BookModule],
+    }),AuthorModule, BookModule, WinstonModule.forRoot(winstonConfig),],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
